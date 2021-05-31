d055c0fd1c5beb728cc734e51eaf00641a84d9f7
Dan Santos <dan.santos@cotalker.com>
2021-05-30 23:29:04 -0400

    Se modifican cosas

--- src/modules/dataset.py
+++ src/modules/dataset.py
@@ -5,8 +5,15 @@ def get_values_dataset():
     df = pd.read_csv('data.csv', index_col=0)
 
     # Crear variables
-    X = df.drop(labels=['song_title', 'artist', 'target'], axis=1).values.tolist()
-    y = df['target'].values.tolist()
+    X = df.drop(labels=['song_title', 'artist', 'target'], axis=1)
+    y = df['target']
 
-    return (X,y)
+    cols_to_normalize = ['duration_ms', 'key', 'tempo', 'time_signature']
 
+    # Normalizar el conjunto de entrada
+    for col in cols_to_normalize:
+      X[col] = df[col] / df[col].max()
+
+    X.loudness = X.loudness / X.loudness.min()
+
+    return (X.values.tolist(), y.values.tolist())
